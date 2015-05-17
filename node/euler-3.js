93e51b296ced6fd3ef858f5acba2b4179ef5bf31
Grant Sheldon <gsheld@gmail.com>
2015-05-17 13:32:37 -0700

    Finished Euler #3.

---
+++ node/euler-3.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    var p = 2,
+        factors = [],
+        n = process.argv[2],
+        colors = require('colors');
+
+    while (n > 1) {
+        while (n % p === 0) {
+            factors.push(p);
+            n /= p; // Divide target by current prime
+        }
+
+        p++; // Increment current prime by one.
+
+        // Only need to check up until sqrt(n) -- if nothing divides it by this
+        // point, n must be prime. Because of this check, this algorithm runs
+        // in O(sqrt(n)) as opposed to O(n).
+        if (p * p > n && n > 1) {
+            console.log(p + ' * ' + p + ' is >' + n + '. It\'s a factor.');
+            factors.push(n);
+            break;
+        }
+    }
+
+    console.log(String(Math.max.apply(null, factors)).bold.green);
+}());
